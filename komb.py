96bcd87c0c58c7a42cc74dc3e66fd3204288fb05
Ronny <ronaldsrundans@gmail.com>
2019-04-25 11:18:19 +0200

    Update komb.py

--- komb.py
+++ komb.py
@@ -2,22 +2,18 @@ def my_function(arr):
 	#print(arr)
 	n=0
 	for i in range(4):
-		for j in range(5):
-			if(i!=j):
-				if(arr[i]==arr[j]):
-					#print(arr)
-					n+=1
+		for j in range(i+1,5):
+			if(arr[i]==arr[j]):
+				n+=1
 			else:continue
+		if(n>0):
+			break
+
 	return n
 #a)Cik iesakas ar "98"?
 #b)Cik dalas ar 5?
 #c)Cik beidzas ar "78"?
-#d)Cik dazadi gadijumi?
-
-	
-	
-  
-
+#d)Cik dazadi gadijumi?	
 from array import *
 a=0
 b=0
@@ -27,27 +23,17 @@ arr = [5,7,8,9,0]
 #print(arr)
 res = [None] *5
 for i in range(4):
-	
 	res[0]=arr[i]
-	#print(arr[i])
 	for j in range(5):
 		res[1]=arr[j]
-		#print(res)
-		#print(arr[j])
 		for k in range(5):
 			res[2]=arr[k]
-			#print(arr[k])
 			for l in range(5):
 				res[3]=arr[l]
-				#print(arr[l])
 				for m in range(5):
 					res[4]=arr[m]
-					#print(res)
-					#c+=1
-
-					if(my_function(res)<1):
-						#print(res)
-						d+=1
+						if(my_function(res)<1):
+							d+=1
 						if(res[0]==9 and res[1]==8):
 							a+=1
 							print("a=")
@@ -60,7 +46,6 @@ for i in range(4):
 							c+=1
 							print("c=")
 							print(res)
-						#print(my_function(res))
 print(a)
 print(b)
 print(c)
