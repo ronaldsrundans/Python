97a1721852f7f4b09214c67d958dff889a43cd32
Ronny <ronaldsrundans@gmail.com>
2020-04-07 11:44:43 +0300

    Update formotion.py

--- formotion.py
+++ formotion.py
@@ -6,25 +6,24 @@ import shutil
 #get the date for this day
 #today=('{date:%Y%m%d}'.format( date=datetime.datetime.now()))
 #create a new folder
-newpath = r'/media/Storage/PythonProgramming/Test/testfolder/'+today 
-if not os.path.exists(newpath):
-    os.makedirs(newpath)
+#newpath = r'/media/Storage/PythonProgramming/Test/testfolder/'+today 
+#if not os.path.exists(newpath):
+#    os.makedirs(newpath)
 #get yesterday date from file
 #f = open("yesterday.txt", "r")   
 #yesterday=f.read()
 #print(yesterday)
 #sort files in yesterday folder
 #f.close()
-olddates={"20200406","20200405","20200404","20200403","20200402","20200401","20200331"}
-#for
+olddates=["20200406","20200405","20200404","20200403","20200402","20200401","20200331"]
+for x in olddates:
+ newpath = r'/var/lib/Motion/'+x
+ if not os.path.exists(newpath):
+  os.makedirs(newpath)
 
-newpath = r'/media/Storage/PythonProgramming/Test/testfolder/'+today
-if not os.path.exists(newpath):
- os.makedirs(newpath)
-
-for data in glob.glob("/media/Storage/PythonProgramming/Test/"+yesterday+"*"):
-    if not os.path.isdir(data):
-        shutil.move(data,"/media/Storage/PythonProgramming/Test/testfolder/"+yesterday)
+ for data in glob.glob("/var/lib/Motion/"+x+"*"):
+  if not os.path.isdir(data):
+   shutil.move(data,"/var/lib/Motion/"+x)
 
 #write the new date for yesterday.txt  
 #f = open("yesterday.txt", "w")  
