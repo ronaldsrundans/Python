614818fdf147803f0817838c571102bbbbfba6e0
ronaldsrundans <ronaldsrundans@gmail.com>
2022-08-28 17:13:19 +0300

    Prints todolist items

--- Django/myworld/todolist/views.py
+++ Django/myworld/todolist/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Todolist
 
 def index(request):
-  template = loader.get_template('myfirst.html')
-  return HttpResponse(template.render())
+  mymembers = Todolist.objects.all().values()
+  output = ""
+  for x in mymembers:
+    output += x["todoitem"]
+  return HttpResponse(output)
