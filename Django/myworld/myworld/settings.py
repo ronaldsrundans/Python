9b8705ce77f4c6386a25c1238fb87386064484a2
ronaldsrundans <ronaldsrundans@gmail.com>
2022-08-28 17:04:32 +0300

    Created model for todolist

--- Django/myworld/myworld/settings.py
+++ Django/myworld/myworld/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'members.apps.MembersConfig',
+    'todolist.apps.TodolistConfig'
 ]
 
 MIDDLEWARE = [
--- Django/myworld/todolist/models.py
+++ Django/myworld/todolist/models.py
@@ -1,3 +1,9 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+#import datetime
+
+class Todolist(models.Model):
+  todoitem = models.CharField(max_length=255)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  deleted_at = models.DateTimeField(auto_now=True)
