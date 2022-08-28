e78c71aa241e38b53ca1a0dbdef5c9384ab9cb8c
ronaldsrundans <ronaldsrundans@gmail.com>
2022-08-28 16:10:23 +0300

    Added new app - todolist

--- Django/myworld/myworld/urls.py
+++ Django/myworld/myworld/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('members/', include('members.urls')),
-    path('admin/', admin.site.urls),
+	path('members/', include('members.urls')),
+	path('admin/', admin.site.urls),
+	path('todolist/', include('todolist.urls')),
 ] 
