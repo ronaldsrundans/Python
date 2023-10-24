05dd9789525cf8f069227243e2982bb4b289c67f
Ronny <ronaldsrundans@gmail.com>
2023-10-24 14:32:42 +0300

    Update crc.py
    Division now works(for this example). Needs more testing

--- DatorikasMagistratura/DatoruTikli/crc.py
+++ DatorikasMagistratura/DatoruTikli/crc.py
@@ -18,24 +18,38 @@ def CRCfunction(frame,generator):
 		frame.append(0)
 	len_frame=len(frame)
 	print("Frame=")
-	for i in frame:
-		print(i)
-#	print("Gen=")
+	#for i in frame:
+	#	print(i)
+	print(frame)
 	arr=[]
 	remainder=[]
-	for i in range(len_remainder):
-		arr.append(frame[i])
-	print(arr)
-	for j in range(len_frame-len_remainder):
-		arr.append(frame[j+len_remainder])
-		for i in range(len_remainder):
-			if (arr[i]==0):
-				break
-			else:
+	#for i in range(len_remainder):
+	#	arr.append(frame[i])
+	#print(arr)
+	for j in range(len_frame):
+		print("j=")
+		print(j)
+		arr.append(frame[j])
+		if(len(arr)==len_remainder):
+			for i in range(len_remainder):
+				print(arr[i])
 				arr[i]=(XORfunction(arr[i],generator[i]))
-			
-		arr.pop(0)
+			arr.pop(0)		
 		print(arr)
+#arr.append(frame[j+len_remainder])
+"""		print("Arr=")
+		print(arr)
+		for i in range(len_remainder):
+			#if (arr[0]==0):
+			#	break
+			#else:
+			print(str(arr[i])+"   XOR "+str(generator[i]))
+			arr[i]=(XORfunction(arr[i],generator[i]))
+			print(arr[i])
+			#print(XORfunction(arr[i],generator[i]))
+		arr.append(frame[j+len_remainder])			
+		arr.pop(0)
+"""		#print(arr)
 		
 #	print(arr)
 			#remainder.append()
@@ -45,7 +59,7 @@ def CRCfunction(frame,generator):
 #			print(XORfunction(frame[i+j],generator[j]))
 			#print(j)
 	
-	return 0
+#	return 0
 word=[1,0,0,1,0,0]
 error=[0,1,0,0,0,0,0]
 key=[1,1,0,1]
