d4f906a1ad45cf50e7c66adc48f22d08d44a8faf
Ronny <ronaldsrundans@gmail.com>
2023-10-27 22:12:43 +0300

    Update ham.py
    A lot of updates

--- DatorikasMagistratura/DatoruTikli/ham.py
+++ DatorikasMagistratura/DatoruTikli/ham.py
@@ -3,9 +3,8 @@ def XORfunction(a,b):
                 return 0
         else:
                 return 1
-def MainFunction(word):
-	parity_pos=[]
-	send_word=[]
+def PaddingParity(word,send_word,parity_pos):
+	#return 0
 	word_len=len(word)
 	#Aizpilda masivu ar paritātes bitu adresēm, kur tos likt sūtāmajā ziņā (0,1,3,...) 
 	for i in range(int(word_len/2+1)):
@@ -16,91 +15,7 @@ def MainFunction(word):
         		if(j==len(send_word)) :
             			send_word.append(j)
     		send_word.append(i)
-	#Aprēķina katra paritātes bita vērtību šajā ciklā
-	for i in parity_pos:
-		sum_par=0
-		k=i+1
-		k_pos=0
-		bin_k=[]
-		
-		for j in range(len(parity_pos)):
-			bin_k.append(k%2)
-			if(k%2==1):
-				break
-			k=int(k/2)
-			k_pos+=1
-		#Meklē pozīcijas sūtāmajā tekstā, kuru adresēs binārā pierakstā atrodas vērtība "1"
-		#Piemēram, ja paritāte 010, tad skatās uz 1010, 1111,0111,...
-		for x in range(i+1,len(send_word),1):
-			l=x+1
-			l_pos=0
-			bin_l=[]
-			for y in range(len(parity_pos)):
-            			bin_l.append(int(l%2))
-				#Ja atrod adresi ar "1" (binārā formā), meklē 001 un atrod 101
-            			if(l_pos==k_pos and l%2==1):
-                			break
-            			l=int(l/2)
-            			l_pos+=1
-			#Skaita "1" simbolus attiecīgajās pozīcijās
-			if(k_pos==l_pos):
-				if(send_word[x]==1):
-                			sum_par+=1
-		#Ja simbolu "1" skaits ir pāra skaitlis, tad raksta "0"
-		if (sum_par%2==0):
-			send_word[i]=0
-		#Ja simbolu "0" skaits ir nepāra skaitlis, tad raksta "1"
-		else:
-        		send_word[i]=1
-	print("transmitted word=")
-	print(send_word)
-	print("received word=")
-"""def AddErorr(): 
-##add error
-	for i in error:
-    		for j in parity_pos:
-               		if(j==len(r_word)) :
-            			r_word.append(0)
-    		r_word.append(i)
-	for i in range(len(r_word)):
-		send_word[i]=XORfunction(r_word[i],send_word[i])
-	print(send_word)
-def FixError():
-#erro correction
-	cars=[]
-	for i in send_word:
-		cars.append(i)
-	for i in parity_pos:
-    		sum_par=0
-    		k=i+1
-    		k_pos=0
-    		bin_k=[]
-    		for j in range(len(parity_pos)):
-        		bin_k.append(k%2)
-        		if(k%2==1):
-            			break
-        		k=int(k/2)
-        		k_pos+=1
-    		for x in range(i+1,len(cars),1):
-        		l=x+1
-        		l_pos=0
-        		bin_l=[]
-        		for y in range(len(parity_pos)):
-            			bin_l.append(int(l%2))
-            			if(l_pos==k_pos and l%2==1):
-                			break
-            			l=int(l/2)
-            			l_pos+=1
-        		if(k_pos==l_pos):
-            			if(cars[x]==1):
-                			sum_par+=1         
-    		if (sum_par%2==0):
-        		cars[i]=0
-    		else:
-        		cars[i]=1
-    #send_
-	print("compared word=")
-	print(cars)
+def ParityCheck(send_word, cars, parity_pos):
 	bin_error=[]
 	for i in parity_pos:
 		bin_error.append(XORfunction(send_word[i],cars[i]))
@@ -117,15 +32,79 @@ def FixError():
 		print("Corrected word:")
 		send_word[error_pos-1]=XORfunction(send_word[error_pos-1],1)
 		print(send_word)
-"""
-word2=[1,1,0,1,1,1,1]
-error=[0,1,0,0,0,0,0]
-MainFunction(word2)
-r_word=[]
-#word_len=len(word)
-#parity_pos=[]
-send_word=[]
-print( "word=")
-print(word2)
-print("Erorr=")
-print(error)
+def ParityValues(send_word,parity_pos):
+	#Aprēķina katra paritātes bita vērtību šajā ciklā
+	for i in parity_pos:
+		sum_par=0
+		k=i+1
+		k_pos=0
+		bin_k=[]
+		#Paritātes bita adresi pārveido binārā skaitlī (0001, 0010, ...)
+		for j in range(len(parity_pos)):
+			bin_k.append(k%2)
+			if(k%2==1):
+				break
+			k=int(k/2)
+			k_pos+=1
+		#Meklē pozīcijas sūtāmajā tekstā, kuru adresēs binārā pierakstā atrodas vērtība "1"
+		#Piemēram, ja paritātes bits ir 0010, tad skatās uz 1010, 1111,0111,...
+		for x in range(i+1,len(send_word),1):
+			l=x+1
+			l_pos=0
+			bin_l=[]
+			for y in range(len(parity_pos)):
+            			bin_l.append(int(l%2))
+				#Ja atrod adresi ar "1" īstaja vietā (binārā formā), meklē 0 001 un atrod 0101
+            			if(l_pos==k_pos and l%2==1):
+                			break
+            			l=int(l/2)
+            			l_pos+=1
+			#Skaita "1" simbolus attiecīgajās pozīcijās
+			if(k_pos==l_pos):
+				if(send_word[x]==1):
+                			sum_par+=1
+		#Ja simbolu "1" skaits ir pāra skaitlis, tad raksta "0"
+		if (sum_par%2==0):
+			send_word[i]=0
+		#Ja simbolu "0" skaits ir nepāra skaitlis, tad raksta "1"
+		else:
+        		send_word[i]=1
+def AddErorr(send_word,error,parity_pos):
+	r_word=[]
+	for i in error:
+		for j in parity_pos:
+			if(j==len(r_word)) :
+				r_word.append(0)
+		r_word.append(i)
+	for i in range(len(r_word)):
+		#Kļūdu rada XOR funkcija, kas pārveido 0->1 vai 1->0, ja Error masīvā ir "1" 
+		send_word[i]=XORfunction(r_word[i],send_word[i])
+	print(send_word)
+def HammingCode(word, error):
+	print("Message:")
+	print(word)
+	parity_pos=[]
+	send_word=[]
+	PaddingParity(word,send_word, parity_pos)
+	ParityValues(send_word, parity_pos)	
+	print("Transmitted word:")
+	print(send_word)
+	print("Error:")
+	print(error)
+	print("Received word:")
+	AddErorr(send_word,error, parity_pos)
+	#Sūtāmajā vārdā pievieno kļūdu(nemaina paritātes bitu vērtības	
+	print(send_word)
+	cars=[]
+	for i in send_word:
+		cars.append(i)
+	#Parbauda kadai jabut Parity vertibam sanemtaja zina
+	ParityValues(cars,parity_pos)
+	print("Compared word:")
+	print(cars)
+	ParityCheck(send_word, cars, parity_pos)
+	
+word_arr=[[1,1,0,1,1,1,1],[1,0,0,1,0,0,0],[1,1,0,0,0,0,1],[1,1,0,1,1,0,1],[1,1,0,1,0,0,1]]
+er_arr=[[0,1,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,1,0],[0,0,0,0,1,0,0],[0,0,0,1,0,0,0]]
+for i in range(5):
+	HammingCode(word_arr[i],er_arr[i])
