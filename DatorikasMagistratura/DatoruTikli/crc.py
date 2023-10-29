fc9d9e17b99aed02f2b16c4cfb47bb11345e3686
Ronny <ronaldsrundans@gmail.com>
2023-10-29 11:00:57 +0200

    Update crc.py
    fixed examples

--- DatorikasMagistratura/DatoruTikli/crc.py
+++ DatorikasMagistratura/DatoruTikli/crc.py
@@ -3,12 +3,8 @@ def XORfunction(a,b):
                 return 0
         else:
                 return 1
-#def comparefunction(a,b):
-#	for i in range(0,4,1):
-#		print(i)
-
 def CRCfunction(frame,generator):
-	print("Message:")
+	print("CRC Message:")
 	print(frame)
 	len_remainder=len(generator)
 	#Pagarina bitu virknes gala pievieno "0" vertibas
@@ -22,23 +18,24 @@ def CRCfunction(frame,generator):
 	for j in range(len_frame):
 		arr.append(frame[j])
 		if(len(arr)==len_remainder):
+			#Dala tikai tad, ja pirmajā pozīcijā ir "1"
 			if(arr[0]==1):
 				for i in range(len_remainder):
-					#print(arr[i])
 					arr[i]=(XORfunction(arr[i],generator[i]))
+			#Ja pirmajā pozīcijā ir "0", tad dala ar virkni "000.."
 			else:
 				for i in range(len_remainder):
 					arr[i]=(XORfunction(arr[i],0))
 			
 			arr.pop(0)		
-		#print(arr)
-		#Nomaina pēdējos ar rezultātu
+	#Nomaina pēdējos bitus ar rezultātu
 	for i in range(0,len(arr), 1):
 		frame[len_frame-len(arr)+i]=arr[i]
 	print("Transmitted message:")
 	print(frame)
 def AddErorr(send_word,error,parity_pos):
 	r_word=[]
+	#Hamminga kodā pievieno paritātes bitus error masīvā, bet CRC neko nedara šis cikls
 	for i in error:
 		for j in parity_pos:
 			if(j==len(r_word)) :
@@ -49,18 +46,9 @@ def AddErorr(send_word,error,parity_pos):
 		send_word[i]=XORfunction(r_word[i],send_word[i])
 	print(send_word)
 def CRCdecode(frame, generator,arr):
-	#print("Decode")
 	len_remainder=len(generator)
-	#for i in range(1,len_remainder,1):
-	#	frame.append(0)
 	len_frame=len(frame)
-	#print("Received message:")
-	#print(frame)
-	#arr=[]
 	remainder=[]
-	#for i in range(len_remainder):
-	#	arr.append(frame[i])
-	#print(arr)
 	for j in range(len_frame):
 		#print("j=")
 		#print(j)
@@ -75,13 +63,7 @@ def CRCdecode(frame, generator,arr):
 				for i in range(len_remainder):
 					arr[i]=(XORfunction(arr[i],0))
 			
-			arr.pop(0)		
-		#print(arr)
-		#Nomaina pēdējos ar rezultātu
-	#for i in range(0,len(arr), 1):
-	#	frame[len_frame-len(arr)+i]=arr[i]
-	#print("Parity check:")
-	#print(arr)
+			arr.pop(0)
 def fixError(frame,generator,arr):
 	#print("Fix error!")
 	tmp=[]
@@ -99,27 +81,23 @@ def fixError(frame,generator,arr):
 		#Nomaina uz pretejo vertibu
 		tmp[i+1]=(XORfunction(tmp[i+1],1))
 		CRCdecode(tmp,generator,checksum)
-		#print("Checksum:")
-		#print(checksum)
 		for j in checksum:
-			#Skaita "0" checksuma
+			#Skaita "0" checksumā
 			if(j==0):
-				#print(j)
 				checkcounter+=1
-		#print(checkcounter)
 		if (len(checksum)==checkcounter):
-			#Ja checksum visi ir "0", tad ir izlabota zina
+			#Ja checksumā visi ir "0", tad ir izlabota saņemtā ziņa
 			print("Fixed error")
-			#print(len(checksum))
 			print(tmp)
 			break
 		else:
 			tmp[i+1]=var
-			print("Done")
+			#print("Done")
 
 def CRC(word,key,error):
 	CRCfunction(word,key)
 	arr=[]
+	fix=0
 	print("Error:")
 	print(error)
 	AddErorr(word,error,[])
@@ -128,13 +106,17 @@ def CRC(word,key,error):
 	CRCdecode(word,key,arr)
 	print("Parity check:")
 	print(arr)
-	fixError(word,key,arr)
-
-
-
-words=[[1,0,0,1,0,0],[1,0,0,0,0,0,0,0,1],[1,1,0,1,0,1,1,0,1,1],[1,0,1,1,0,0,1,1]]
+	for i in arr:
+		if(i==1):
+			fix=1
+	if (fix==1):
+		fixError(word,key,arr)
+	fix=0
+	print("\n")
+###CRC# EXAMPLES
+words=[[1,0,0,1,0,0],[1,1,0,1,0,1,1,0,1,1],[1,1,0,1,0,1,1,0,1,1],[1,0,1,1,0,0,1,1],[1,0,0,1,0,0]]
 keys=[[1,1,0,1],[1,0,0,1,1],[1,0,0,1,1],[1,0,0,1,1],[1,0,0,1,1]]
-errors=[[0,1,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,1,0,0,0,0,0,0,0],[0,1,0,0,0,0,0,0,0]]
+errors=[[0,1,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,1,0,0,0,0,0,0,0]]
 for i in range(5):
-	print(i)
 	CRC(words[i], keys[i], errors[i])
+	("Done")
