19dbbd78b145b5934f7ea75a69d364c5c34e7e6e
Ronny <ronaldsrundans@gmail.com>
2023-12-28 13:31:16 +0200

    Update md5.py
    Hash values

--- DatorikasMagistratura/DatoruTikli/MD3/md5.py
+++ DatorikasMagistratura/DatoruTikli/MD3/md5.py
@@ -1,19 +1,16 @@
 
-#https://www.comparitech.com/blog/information-security/md5-algorithm-with-examples/
-# “They are deterministic”  hash of 23db6982caef9e9152f1a5b2589e6ca3 every single time.
-import math
-def string2bin(plaintext):
+def string2bin(plaintext): #string to binary
 	#print()
 	res = ''.join(format(ord(i), '08b') for i in plain_text)
 	return res
-def bin2string(binres):
+def bin2string(binres): #binary to string
 	str = ""
 	for i in range(0, len(binres), 8):
 		binc = binres[i:i + 8]
 		num = int(binc, 2)
 		str += chr(num)
 	return str
-def num2bin(num):
+def num2bin(num):#dec to bin
 	x=bin(num)[2:]
 	return x
 def hex2bin(hex_num):#Takes only 2 letters/numbers like "ff" or "00" LOWERCASE!!!
@@ -23,26 +20,22 @@ def hex2bin(hex_num):#Takes only 2 letters/numbers like "ff" or "00" LOWERCASE!!
 		binary += hex_dict[digit]
 	binary=list(binary)
 	return binary
-def fullHex2Bin(b,arr):
+def fullHex2Bin(b,arr): # convert binary arr (len 32)to Hex  
 	for i in range(4):
 		tmp=[]
 		tmp=hex2bin(b[0+i*2:2+i*2].lower())
 		for j in range(8):
 			arr.append(tmp[j])
-def bin2hex(n):
-    # convert binary to int
-    num = int(n, 2)
-    # convert int to hexadecimal
-    hex_num = format(num, 'x')
+def bin2hex(n):   
+    num = int(n, 2) # convert binary to int    
+    hex_num = format(num, 'x')# convert int to hexadecimal
     return(hex_num)
-def ANDfunction(x,y):
-	#if(x=='1' and y=='1'):
+def ANDfunction(x,y):	
 	if(int(x)&int(y)):
 		return '1'
 	else:
 		return '0'
-def ORfunction(x,y):
-	#if(x=='1' or y=='1'):
+def ORfunction(x,y):	
 	if(int(x)|int(y)):
 		return '1'
 	else:
@@ -57,7 +50,7 @@ def XORfunction(x,y):
 		return '1'
 	else:
 		return '0'
-def ffunction(B,C,D):
+def ffunction(B,C,D):#F  (B AND C) OR (NOT B AND D)
 	arr=[]
 	arr2=[]
 	arr3=[]
@@ -71,8 +64,7 @@ def ffunction(B,C,D):
 	for i in range(32):
 		arr4.append(ORfunction(arr[i],arr3[i]))
 	return arr4
-def gfunction(B,C,D):
-	#(B AND D)OR(C AND NOT(D))
+def gfunction(B,C,D):	#(B AND D)OR(C AND NOT(D))
 	arr=[]
 	arr2=[]
 	arr3=[]
@@ -86,8 +78,7 @@ def gfunction(B,C,D):
 	for i in range(32):
 		arr4.append(ORfunction(arr[i],arr3[i]))
 	return arr4
-def hfunction(B,C,D):
-	#(B XOR C XOR D)
+def hfunction(B,C,D):	#(B XOR C XOR D)
 	arr=[]
 	arr3=[]
 	for i in range(32):
@@ -96,8 +87,7 @@ def hfunction(B,C,D):
 		arr3.append(XORfunction(arr[i],D[i]))
 	return arr3
 
-def ifunction(B,C,D):
-#“C OR, BUT NOT BOTH (B OR NOT-D)
+def ifunction(B,C,D): #“C OR, BUT NOT BOTH (B OR NOT-D)
 	arr=[]
 	arr2=[]
 	arr3=[]
@@ -108,10 +98,10 @@ def ifunction(B,C,D):
 	for i in range(32):
 		arr3.append(XORfunction(C[i],arr2[i]))
 	return arr3
-def hexCheck(arr):#len(arr) must be 32 (bin) => 2hex values 
+def hexCheck(arr):#len(arr) must be 32bits (bin form) => 2 hex values 
 	for i in range(8):#4*8=32
 		print(bin2hex((arr[i*4])+(arr[i*4+1])+(arr[i*4+2])+(arr[i*4+3])))
-def modSum(a,b):
+def modSum(a,b):# Modular addition
 	c=[]
 	for i in range(32):
 		c.append('0')
@@ -121,7 +111,7 @@ def modSum(a,b):
 		if((i>0)and(x>1)):
 		    c[i-1]=1
 	return c
-def leftBitShift(arr,n):
+def leftBitShift(arr,n):#shift n first bits to end of arr
 	tmp=[]
 	for i in range(n):
 		tmp.append(arr[i])
@@ -166,85 +156,60 @@ def isItPrime (num):#Is this number a prime number?
 		if (num % i == 0):
 			return False
 	return True
-def gcd(a, h):
+def gcd(a, h):#Greatest Common Divider
     temp = 0
     while(1):
         temp = a % h
         if (temp == 0):
             return h
         a = h
-        h = temp
-		# To encrypt the given number
-def encrypt(message, e,n):
-    #global public_key, n
-    #e = public_key
+        h = temp		
+def encrypt(message, e,n):# To encrypt the given number
     encrypted_text = 1
     while e > 0:
         encrypted_text *= message
         encrypted_text %= n
         e -= 1
     return encrypted_text
- 
- 
-# To decrypt the given number
-def decrypt(c,d,n):
-    #global private_key, n
-    #d = private_key
+def decrypt(c,d,n):# To decrypt the given number
+
     decrypted = 1
     while d > 0:
         decrypted *= c
         decrypted %= n
         d -= 1
     return decrypted
-p=191#53 #3
-print(isItPrime(53))
-q=163#59 #11 #59
+#RSA algoritms
+p=191#
+q=163#
+#print(isItPrime(191))
+#print(isItPrime(163))
 n=p*q #must be >256
 e=2	# e*d=1(mod z)# 2 # >1 and not a factor of z
-z=(p-1)*(q-1)	#3016
+z=(p-1)*(q-1)	
 k = 1 #konstante pie d aprēķina
 d=0
 while (e < z):
 	if(gcd(e, z) == 1):
-		print(e)
 		for k in range(1,n):
-			d = (1 + (k*z))/e #d ir vesels skaitlis
-			if(d%1==0):
-				print("k=", k)
+			d = (1 + (k*z))/e 
+			if(d%1==0):#d ir vesels skaitlis
 				break
 		if(d%1==0):
 			break
-		e = e+1
+		e = e+1#citādi bezgalīgs cikls
 	else:
 		e = e+1
-print("p=",p)	
-print("q=",q)	
-print("n=",n)
-print("e=",e)
-# Private key (d stands for decrypt)
-# choosing d such that it satisfies
-# d*e = 1 + k * totient
- 
-
-#d = (1 + (k*z))/e
-#d=7# (1 + (k*z))/e , k=2 #2011
-print("d=",d)
-
-#print(p,q,n,z,d,e)	
-#Public key pair (e,n)=(3,20)
-#Private key pair (d,n)=(7,20)
-#Encryption P^e(mod n)
-#Decryption C^d(mod n)
-#c = (msg ^ e) % n
+#Public key pair (e,n)
+#Private key pair (d,n)
+		
+#/RSA algoritms
+#MD5 algoritms
 plain_text="KINO"
-#plain_text="They are deterministic"
-#print("The message is : " + str(plain_text))
-res=string2bin(plain_text) 
-#print("The string after binary conversion : " + str(res))
-#1. Append Padding Bits:
+res=string2bin(plain_text) #string after binary conversion : " + str(res))
 l_res=len(res) 
 res=res+'1'
-for x in range(l_res,511-64):
+for x in range(l_res,511-64):#1. Append Padding Bits:
 	res=res+'0'
 l_bin=num2bin(l_res)
 l_bin2=len(l_bin)
@@ -252,7 +217,7 @@ for y in range(l_bin2, 64):
 	res=res+'0'
 res=res+l_bin
 str=bin2string(res)
-#print("Plain text: ", str)
+print("Plain text: ", plain_text)
 res=list(res)
 Karr=["D76AA478","E8C7B756","242070DB","C1BDCEEE","f57c0faf","4787C62A","A8304613","FD469501","698098D8","8B44F7AF","FFFF5BB1","895CD7BE","6B901122","FD987193","A679438E","49B40821","F61E2562","C040B340","265E5A51","E9B6C7AA","D62F105D","02441453","D8A1E681","E7D3FBC8","21E1CDE6","C33707D6","F4D50D87","455A14ED","A9E3E905","FCEFA3F8","676F02D9","8D2A4C8A","FFFA3942","8771F681","699D6122","FDE5380C","A4BEEA44","4BDECFA9","F6BB4B60","BEBFBC70","289B7EC6","EAA127FA","D4EF3085","04881D05","D9D4D039","E6DB99E5", "1FA27CF8","C4AC5665","F4292244","432AFF97","AB9423A7","FC93A039","655B59C3","8F0CCC92","FFEFF47D","85845DD1","6FA87E4F","FE2CE6E0","A3014314","4E0811A1","F7537E82","BD3AF235","2AD7D2BB","EB86D391"]
 Sarr=[7,12,17,22,7,12,17,22,7,12,17,22,7,12,17,22,5,9,14,20,5,9,14,20,5,9,14,20,5,9,14,20,4,11,16,13,4,11,16,13,4,11,16,13,4,11,16,13,6,10,15,21,6,10,15,21,6,10,15,21,6,10,15,21]
@@ -262,8 +227,7 @@ for i in range(16):
 	tmp=[]
 	for j in range(32):
 		tmp.append(res[i*32+j])
-	M_arr.append(tmp)
-	
+	M_arr.append(tmp)	
 A_bin=[]
 B_bin=[]
 C_bin=[]
@@ -284,7 +248,6 @@ fullHex2Bin("76543210", D_bin0)
 for r in range(0,16):
 	arr_F=(ffunction(B_bin, C_bin, D_bin))# F function
 	arr_mS=(modSum(A_bin,arr_F))# 		#A+F
-	#print("Marr=",Marr[r])
 	arr_mS0=(modSum(M_arr[Marr[r]],arr_mS)) #M value	A+M
 	K_bin=[]           
 	fullHex2Bin(Karr[r], K_bin) #K value
@@ -330,42 +293,17 @@ A_bin=(modSum(A_bin,A_bin0))
 B_bin=(modSum(B_bin,B_bin0))
 C_bin=(modSum(C_bin,C_bin0))
 D_bin=(modSum(D_bin,D_bin0))
-
-first_byte=[]
+#/MD5 algoritms
+first_byte=[]#Pirmais bits no MD5
 for i in range(8):
 	first_byte.append(A_bin[i])
 print("First byte=",first_byte)
-
-#print(bin2hex((first_byte[0])+(first_byte[1])+(first_byte[2])+(first_byte[3])))
-#print(bin2hex((first_byte[4])+(first_byte[5])+(first_byte[6])+(first_byte[7])))
-# Message to be encrypted
-#msg = 170.0
+#RSA algoritms
 msg=int(first_byte[7])+2*int(first_byte[6])+4*int(first_byte[5])+8*int(first_byte[4])+16*int(first_byte[3])+32*int(first_byte[2])+64*int(first_byte[1])+128*int(first_byte[0])
-print("Message data(dec) = ", msg)
+print("Message Hash value (dec) = ", msg)
 c=encrypt(msg, e,n)
-print(c)
-msg=decrypt(c, d,n)
-
-# Encryption c = (msg ^ e) % n
-#c=pow(msg,e)#
-#c = math.fmod(pow(msg,e), n)
-#print(type(c))
-#msg=c%n
-#print(n)
-#c=c%n
+print("Signed message = ", plain_text,c)
 #print(c)
-#c = #math.fmod(c, n)
-#print("Encrypted data = ", c)
- 
-# Decryption m = (c ^ d) % n
-#msg=math.fmod(pow(c,d),n)
-#print(msg)
-#msg=msg%n
-print("Message:",msg)
-#m = pow(c, d)
-#m = math.fmod(m, n)
-#print("Original Message Sent = ", m)
-#print(encrypt(msg, e,n))#28858
-#print(decrypt(28858.0, d,n))
-#decrypt(c,d,n):
-#print(~1)
+msg=decrypt(c, d,n)
+print("Decrypted Hash value:",msg)
+#/RSA algoritms
