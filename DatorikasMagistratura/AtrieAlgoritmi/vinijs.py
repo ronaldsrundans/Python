bc57862533ced09e642c54463e2441e6a1c97b5a
Ronny <ronaldsrundans@gmail.com>
2024-01-05 12:14:43 +0200

    Update vinijs.py

--- DatorikasMagistratura/AtrieAlgoritmi/vinijs.py
+++ DatorikasMagistratura/AtrieAlgoritmi/vinijs.py
@@ -1,20 +1,3 @@
-
-"""
-# Python3 program to print all the cycles
-# in an undirected graph
-N = 15
- 
-# variables to be used
-# in both functions
-graph = [[] for i in range(N)]
-cycles = [[] for i in range(N)]
-weights = [[] for i in range(N)]
-rows, cols = (N, N)
-arr = [[]*cols]*rows
-print(arr)
-
-
-""" 
 # Function to mark the vertex with
 # different colors for different cycles
 def dfs_cycle(u, p, color: list,
@@ -64,12 +47,10 @@ def dfs_cycle(u, p, color: list,
 def addEdge(u, v, w): 
     graph[u].append(v)
     graph[v].append(u)
-    print(u,v)
-
+    #print(u,v)
     weights[u].append([v,w])
     weights[v].append([u,w])
-
-    print()
+#    print()
     #print(graph)
 # Function to print the cycles
 def printCycles():
@@ -82,40 +63,72 @@ def printCycles():
         for x in cycles[i]:
             print(x, end = " ")
         print()
+def printCycles2():
+ 
+    # print all the vertex with same cycle
+    for i in range(0, cyclenumber):
+        m=101 
+        # Print the i-th cycle
+        #print("Cycle Number %d:" % (i+1), end = " ")
+        for x in range(len(cycles[i])-1):
+            print(x, end = "\n")
+            #print("Weights for", cycles[i][x])
+            #print("Weights for", cycles[i][x+1])
+
+            num1=cycles[i][x]
+            num2=cycles[i][x+1]
+            #print("Weights for", weights[num1])
+            #print("Weights for", weights[num2])
+            for j in weights[num1]:
+                #print(j[0])
+                if(j[0]==num2):
+                    #print("Found it!:",num2)
+                    
+                    if(j[1]<m):
+                        print("Weight is", j[1])
+                        m=j[1]
+                        print("a1",num1)
+                        print("a2",num2)
+                    break
+
+            print()
+
+        print()
 
 # Python3 program to print all the cycles
 # in an undirected graph
-N = 15
+N = 500
  
 # variables to be used
 # in both functions
 graph = [[] for i in range(N)]
 cycles = [[] for i in range(N)]
 weights = [[] for i in range(N)]
-print(weights)
-adj={}
-rows, cols = (N, N)
-arr = [['x']*cols]*rows
-#arr[0][0]=100
-#print(arr[0][0])
-print(arr)
+#print(weights)
+"""
+for i in range (N-1):
+    addEdge(i,i+1,100)
 
     # add edges
-addEdge(1, 2,1)
+print("weights")
+print(weights[0])
+print(weights[N-1])
+"""
+addEdge(1, 2,-1)
 addEdge(2, 3,2)
 addEdge(3, 4,2)
 addEdge(4, 6,3)
 addEdge(4, 7,4)
-addEdge(5, 6,5)
+addEdge(5, 6,-5)
 addEdge(3, 5,6)
 addEdge(7, 8,7)
 addEdge(6, 10,1)
 addEdge(5, 9,2)
 addEdge(10, 9,3)
-print("arr1=",arr[1][2])
-print("arr2=",arr[2][1])
 
-print(arr)
+#print("weights")
+#print(weights[2])
+
     #addEdge(10, 11)
     #addEdge(11, 12)
     #addEdge(11, 13)
@@ -133,8 +146,9 @@ cyclenumber = 0
 dfs_cycle(1, 0, color, par)
  
     # function to print the cycles
-printCycles()
-print(weights)
+printCycles2()
+
+print(cycles)
 """
 # Driver Code
 if __name__ == "__main__":
